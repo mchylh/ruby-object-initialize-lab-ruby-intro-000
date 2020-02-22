f14eb08a224068995d4dd4dc6a603f679457d990
Michaylah Mam <mchylh@gmail.com>
2020-02-22 20:39:46 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,28 @@
+class Person
+
+  def initialize=(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
+
+
+  def initialize=(job)
+    @job = job
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
